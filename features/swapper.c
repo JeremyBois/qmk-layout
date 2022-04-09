971214dd19afb7e8ad2217f1bc1137671e187f6b
JeremyBois <JeremyBoisGit@pm.me>
2022-04-09 13:22:56 +0200

    ✨ - Add swapper support for mod tap with custom keycode.

--- features/swapper.c
+++ features/swapper.c
@@ -1,18 +1,35 @@
 #include "swapper.h"
 
-void update_swapper(bool *active, bool *old_active, uint16_t cmdish, uint16_t tabish, uint16_t trigger,
-                    uint16_t keycode, keyrecord_t *record) {
-    *old_active = *active;  // Keep track of old state
+void update_swapper(bool *active, bool *old_active, uint16_t cmdish, uint16_t tabish, uint16_t trigger, uint16_t keycode, keyrecord_t *record) {
+    *old_active = *active; // Keep track of old state
     if (keycode == trigger) {
-        if (record->event.pressed) {
-            if (!*active) {
-                *active = true;
-                register_code(cmdish);
-            }
-            register_code(tabish);
-        } else {
-            unregister_code(tabish);
-            // Don't unregister cmdish until some other key is hit or released.
+        switch (keycode) {
+            // Handle custom keycode detection when using mod tap
+            // https://docs.qmk.fm/#/mod_tap?id=intercepting-mod-taps
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+                if (record->event.pressed && record->tap.count) {
+                    if (!*active) {
+                        *active = true;
+                        register_code(cmdish);
+                    }
+                    register_code(tabish);
+                } else {
+                    unregister_code(tabish);
+                    // Don't unregister cmdish until some other key is hit or released.
+                }
+                break;
+            default:
+                if (record->event.pressed) {
+                    if (!*active) {
+                        *active = true;
+                        register_code(cmdish);
+                    }
+                    register_code(tabish);
+                } else {
+                    unregister_code(tabish);
+                    // Don't unregister cmdish until some other key is hit or released.
+                }
+                break;
         }
     } else if (*active) {
         unregister_code(cmdish);
